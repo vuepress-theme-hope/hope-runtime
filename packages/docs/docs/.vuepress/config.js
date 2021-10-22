6ca6897b44db83c828c00a5e6c7b3e379736d2a1
Mr.Hope <zhangbowang1998@gmail.com>
2021-10-22 19:37:50 +0800

    ci: add docs deploy

--- packages/docs/docs/.vuepress/config.js
+++ packages/docs/docs/.vuepress/config.js
@@ -1,6 +1,7 @@
 const { fs, path } = require('@vuepress/shared-utils')
 
 module.exports = ctx => ({
+  base: 'hope-runtime',
   dest: '../../vuepress',
   locales: {
     '/': {
@@ -19,10 +20,29 @@ module.exports = ctx => ({
     ['link', { rel: 'manifest', href: '/manifest.json' }],
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-    ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }],
-    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
-    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    [
+      'meta',
+      { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }
+    ],
+    [
+      'link',
+      { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }
+    ],
+    [
+      'link',
+      {
+        rel: 'mask-icon',
+        href: '/icons/safari-pinned-tab.svg',
+        color: '#3eaf7c'
+      }
+    ],
+    [
+      'meta',
+      {
+        name: 'msapplication-TileImage',
+        content: '/icons/msapplication-icon-144x144.png'
+      }
+    ],
     ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
   ],
   theme: '@vuepress/vue',
@@ -31,13 +51,15 @@ module.exports = ctx => ({
     editLinks: true,
     docsDir: 'packages/docs/docs',
     // #697 Provided by the official algolia team.
-    algolia: ctx.isProd ? ({
-      apiKey: '3a539aab83105f01761a137c61004d85',
-      indexName: 'vuepress',
-      algoliaOptions: {
-        facetFilters: ['tags:v1']
+    algolia: ctx.isProd
+      ? {
+        apiKey: '3a539aab83105f01761a137c61004d85',
+        indexName: 'vuepress',
+        algoliaOptions: {
+          facetFilters: ['tags:v1']
+        }
       }
-    }) : null,
+      : null,
     smoothScroll: true,
     locales: {
       '/': {
@@ -50,7 +72,11 @@ module.exports = ctx => ({
         sidebar: {
           '/api/': getApiSidebar(),
           '/guide/': getGuideSidebar('Guide', 'Advanced'),
-          '/plugin/': getPluginSidebar('Plugin', 'Introduction', 'Official Plugins'),
+          '/plugin/': getPluginSidebar(
+            'Plugin',
+            'Introduction',
+            'Official Plugins'
+          ),
           '/theme/': getThemeSidebar('Theme', 'Introduction')
         }
       },
@@ -72,37 +98,43 @@ module.exports = ctx => ({
   },
   plugins: [
     ['@vuepress/back-to-top', true],
-    ['@vuepress/pwa', {
-      serviceWorker: true,
-      updatePopup: true
-    }],
+    [
+      '@vuepress/pwa',
+      {
+        serviceWorker: true,
+        updatePopup: true
+      }
+    ],
     ['@vuepress/medium-zoom', true],
-    ['@vuepress/google-analytics', {
-      ga: 'UA-128189152-1'
-    }],
-    ['container', {
-      type: 'vue',
-      before: '<pre class="vue-container"><code>',
-      after: '</code></pre>'
-    }],
-    ['container', {
-      type: 'upgrade',
-      before: info => `<UpgradePath title="${info}">`,
-      after: '</UpgradePath>'
-    }],
+    [
+      '@vuepress/google-analytics',
+      {
+        ga: 'UA-128189152-1'
+      }
+    ],
+    [
+      'container',
+      {
+        type: 'vue',
+        before: '<pre class="vue-container"><code>',
+        after: '</code></pre>'
+      }
+    ],
+    [
+      'container',
+      {
+        type: 'upgrade',
+        before: info => `<UpgradePath title="${info}">`,
+        after: '</UpgradePath>'
+      }
+    ],
     ['flowchart']
   ],
-  extraWatchFiles: [
-    '.vuepress/nav/en.js',
-    '.vuepress/nav/zh.js'
-  ]
+  extraWatchFiles: ['.vuepress/nav/en.js', '.vuepress/nav/zh.js']
 })
 
 function getApiSidebar () {
-  return [
-    'cli',
-    'node'
-  ]
+  return ['cli', 'node']
 }
 
 function getGuideSidebar (groupA, groupB) {
